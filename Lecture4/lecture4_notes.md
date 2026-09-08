6dc09de751b429f3c26b35877377bb8b72a9e651
Christian Camp <crdcamp@gmail.com>
2026-09-08 11:48:06 -0600

    Some notes

---
+++ Lecture4/lecture4_notes.md
@@ -0,0 +1,3 @@
+[Lecture Video](https://www.youtube.com/watch?v=db0H0U13YsA&t=63s)
+
+52 - 10:21
---
+++ Lecture4/messin_around_with_pointers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int n = 50;
+    // `*p` stores the address of `n` (an integer), not an actual integer
+    // `&` is essentially telling the computer "tell me the address of `n`"
+    int *p = &n;
+
+    printf("n: %i\n", n);
+    printf("n address: %p\n", &n);
+
+    printf("*p: %i\n", *p);
+    printf("*p address: %p\n", p);
+}
