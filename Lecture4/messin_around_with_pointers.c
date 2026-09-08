bcaa693b0f4427dff17af374ad75be4d10b86ffe
Christian Camp <crdcamp@gmail.com>
2026-09-08 11:51:46 -0600

    Notes

--- Lecture4/messin_around_with_pointers.c
+++ Lecture4/messin_around_with_pointers.c
@@ -2,7 +2,7 @@
 
 int main(void) {
     int n = 50;
-    // `*p` stores the address of `n` (an integer), not an actual integer
+    // `*p stores the address of `n` (an integer), not an actual integer
     // `&` is essentially telling the computer "tell me the address of `n`"
     int *p = &n;
 
