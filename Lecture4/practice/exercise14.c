9a00c007a81b8aa7934619c55014d445368f8d6d
Christian Camp <crdcamp@gmail.com>
2026-09-13 14:24:30 -0600

    Exercises up to 14 complete

---
+++ Lecture4/practice/exercise14.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Use a pointer to iterate through a string and print
+// each character on a new line, stopping at the null terminator.
+void print_each_line(char *message);
+
+int main(void) {
+     char *message = "C Pointers";
+     print_each_line(message);
+}
+
+void print_each_line(char *message) {
+    char *m_p = message;
+    while (*m_p != '\0') {
+        printf("%c\n", *m_p);
+        m_p++; // Iterate to next char
+    }
+}
