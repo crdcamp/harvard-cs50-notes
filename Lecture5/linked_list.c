bc2b3f8cb5a7d0a7e92c3371cc66691550c086e5
Christian Camp <crdcamp@gmail.com>
2026-09-19 21:40:24 -0400

    Some more very interesting linked list things

--- Lecture5/linked_list.c
+++ Lecture5/linked_list.c
@@ -8,5 +8,20 @@ typedef struct node {
 
 int main(void) {
     // Create an empty list
-    node *list = NULL; // The list is the address of the first node in the list
+    node *list = NULL; // The list points to nothing
+
+    for (int i = 0; i < 3; i++) {
+        // First we need to allocate a new node
+        // Create one new node, store a value in it, and then point the list at the node
+        node *n = malloc(sizeof(node)); // Allocate space for the size of the node (we use node * because we want to point to the memory of an actual node)
+        if (n == NULL) {
+            return 1;
+        }
+        // Go to the address of n, go inside that node, and store a value
+        // (previously we used (*n).number instead of n->number)
+        // The ()s are used because of precedence. You need to dereference `n`
+        // before you actually access the value.
+        n->number = i;
+        n->next = NULL;
+    }
 }
